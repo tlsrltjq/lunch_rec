5690be65dbf999a024d80e57582f5c3da79322b1
shingiseop <psl87531@gmail.com>
2025-09-09 11:06:32 +0900

    ch3

--- src/main/java/com/example/lunch_rec/MenuController.java
+++ src/main/java/com/example/lunch_rec/MenuController.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 @WebServlet({"/post","/list"})
 public class MenuController extends HttpServlet {
-    private MenuService menuService = new MenuService();
+    private MenuService menuService = new MenuService(); //서비스 객체 생성
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) //post 요청 처리
             throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");//한글
 
@@ -22,9 +22,8 @@ public class MenuController extends HttpServlet {
         String spice = req.getParameter("spice");
 
         Menu menu = new Menu(m_name, spice, price);
-        menuService.addMenu(menu); //서비스에서 메뉴가져오기
+        menuService.addMenu(menu); //서비스클래스를 통해 메뉴 추가
 
-        //res.sendRedirect("/list.jsp");
         res.sendRedirect(req.getContextPath() + "/list");
     }
 
@@ -32,7 +31,7 @@ public class MenuController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
-        List<Menu> menus = menuService.getMenuList();
+        List<Menu> menus = menuService.getMenuList(); //서비스에서 메뉴 조회
         req.setAttribute("menuList", menus);
 
         req.getRequestDispatcher("/list.jsp").forward(req, res);
--- src/main/webapp/form.jsp
+++ src/main/webapp/form.jsp
@@ -10,16 +10,16 @@
     메뉴이름 : <input type="text" name="m_name"><br>
     가격 :
     <select id="price" name="price">
-        <option value="0">0-10000</option>
-        <option value="1">10000-30000</option>
-        <option value="2">30000-50000</option>
-        <option value="3">50000-</option>
+        <option value="0-10000">0-10000</option>
+        <option value="10000-30000">10000-30000</option>
+        <option value="30000-50000">30000-50000</option>
+        <option value="50000-">50000-</option>
     </select>
     맵기 :
         <select id="spice" name="spice">
-            <option value="0">매움</option>
-            <option value="1">보통</option>
-            <option value="2">순함</option>
+            <option value="매움">매움</option>
+            <option value="보통">보통</option>
+            <option value="순함">순함</option>
         </select>
     <button type="submit">등록</button>
 </form>
